f1be2eeaaf6391327a3898a7b3350e4ecb46a9f9
Tony <Tony@Tony-PC>
2021-12-03 14:01:11 -0500

    First commit

---
+++ src/NameParserApp.java
@@ -0,0 +1,16 @@
+// Tony Rosasco
+// 12/3/21
+
+import java.util.Scanner;
+
+public class NameParserApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a name: ");
+        String name = sc.nextLine();
+        System.out.println();
+        
+    }
+}
